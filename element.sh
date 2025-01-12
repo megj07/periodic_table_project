d1708278eb6fb2a6aef5e5c0408fff147a181716
meg m <mmcvay89@gmail.com>
2025-01-12 18:44:00 +0000

    feat: add basic element information output

--- element.sh
+++ element.sh
@@ -5,3 +5,6 @@ then
   echo "Please provide an element as an argument."
   exit
 fi
+
+# successful output for testing
+echo "The element with atomic number $1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
