887116dcfc99864e905f415e435703946b6015a1
meg m <mmcvay89@gmail.com>
2025-01-12 18:47:33 +0000

    feat: add error handling for invalid elements

--- element.sh
+++ element.sh
@@ -7,4 +7,9 @@ then
 fi
 
 # successful output for testing
-echo "The element with atomic number $1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
+if [[ "$1" == "1" || "$1" == "H" || "$1" == "Hydrogen" ]]
+then
+  echo "The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
+else
+  echo "I could not find that element in the database."
+fi
